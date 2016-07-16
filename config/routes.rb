c61c2c97678e28ef5b992f9eb1528e14b1fffe6a
yutaka karube <yutaka karube>
2016-07-16 18:31:25 +0900

    convert users controller to devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
   root 'prototypes#index'
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
