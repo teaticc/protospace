f44cc8ba5041db2289d78f81decee4a9b0b9b576
yutaka karube <yutaka karube>
2016-07-16 20:33:56 +0900

    sign up function created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  devise_for :users
+ # , :controllers => {:registrations => "users/registrations"}
   root 'prototypes#index'
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
