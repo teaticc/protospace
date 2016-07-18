420aad190bcb0335d7c15be559d16c664ee4824a
yutaka karube <yutaka karube>
2016-07-18 18:35:43 +0900

    add edited function to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-end
+
+  def edit
+     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(name: user_params[:name], email: user_params[:email], member:user_params[:member], profile: user_params[:profile], avatar: user_params[:avatar], works: user_params[:works])
+    redirect_to :root
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,:member, :profile, :avatar, :works)
+    end
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
   get  '/prototypes/show'   =>    'prototypes#show'
-  resources :users, only: [:edit, :show, :index]
+  resources :users, only: [:edit, :show, :index, :update]
 end
