75a23d002430b0c26fb10960a6356d716b2f3d6a
nori <nori@nori-MacBook-Pro.local>
2016-07-18 14:00:29 +0900

    remake user controller and add pry

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
   get  '/prototypes/show'   =>    'prototypes#show'
-  resources :users, only: :edit
+  resources :users, only: [:edit, :show, :index]
 end
