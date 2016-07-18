3240c7ce124c33a6289c8e20cf5c2a6f67c27ad9
nori <nori@nori-MacBook-Pro.local>
2016-07-18 19:21:43 +0900

    Add flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update_attributes(name: user_params[:name], email: user_params[:email], member:user_params[:member], profile: user_params[:profile], avatar: user_params[:avatar], works: user_params[:works])
-    redirect_to :root
+    redirect_to :root, notice: "successfully updated!"
   end
 
   private
@@ -18,5 +18,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,:member, :profile, :avatar, :works)
     end
-  end
 
+end
