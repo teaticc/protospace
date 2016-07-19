bce51bc9562f4f71a7527854a1234d53e7325bf8
nori <nori@nori-MacBook-Pro.local>
2016-07-19 19:02:37 +0900

    Add dependent

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :captured_images
+  has_many :captured_images, dependent: :destroy
   accepts_nested_attributes_for :captured_images
   validates :title, presence: true
   validates :copy, presence: true
