86eb507ef860eb0b13de373072b6286db8393c46
nori <nori@nori-MacBook-Pro.local>
2016-07-19 19:18:24 +0900

    Add validation

--- app/uploaders/captured_image_uploader.rb
+++ app/uploaders/captured_image_uploader.rb
@@ -2,7 +2,7 @@
 
 class CapturedImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -14,6 +14,8 @@ class CapturedImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  process :resize_to_limit => [730,500]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -37,14 +39,14 @@ class CapturedImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "something.jpg" if original_filename
+  end
 
 end
