d44b5b3f59a5db1453f5b614cad75c107078628a
nori <nori@nori-MacBook-Pro.local>
2016-07-19 16:30:04 +0900

    Add Prototype posting function(not finish)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,13 +3,20 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
+    @prototype.captured_images.build
   end
 
   def show
   end
 
+  def create
+    binding.pry
+    redirect_to :root
+  end
+
   private
   def prototype_params
-    params.require(:prototype).permit(:copy, :concept, :image, :titile, :user_id)
+    params.require(:prototype).permit(:copy, :concept, :image, :title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
- # , :controllers => {:registrations => "users/registrations"}
   root 'prototypes#index'
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
+  resource :prototypes, only: [:index, :show, :new, :create]
   resources :users, only: [:edit, :show, :index, :update]
 end
