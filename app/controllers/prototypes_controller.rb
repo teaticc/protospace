7233be6242d1ddf459f7bde4ef5b1a000d79a0e9
nori <nori@nori-MacBook-Pro.local>
2016-07-19 18:52:10 +0900

    Add if else method and flash message

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -11,11 +11,16 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    Prototype.create(prototype_params)
-    redirect_to :root
+    prototype = Prototype.new(prototype_params)
+    if prototype.save
+      redirect_to :root
+    else
+      redirect_to new_prototypes_path, alert: "登録内容に不備があります"
+    end
   end
 
   private
+
   def prototype_params
     params.require(:prototype).permit(:copy, :concept, :title, captured_images_attributes: [:img_url, :img_type]).merge({user_id: current_user.id})
   end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,5 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :captured_images
   accepts_nested_attributes_for :captured_images
+  validates :title, presence: true
 end
