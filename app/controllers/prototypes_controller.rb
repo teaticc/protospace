d434edd620e095eb743b9dde3eca5ec2ae3dc56d
nori <nori@nori-MacBook-Pro.local>
2016-07-19 18:10:52 +0900

    Add save function for prototype

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -12,11 +12,12 @@ class PrototypesController < ApplicationController
 
   def create
     binding.pry
+    Prototype.create(prototype_params)
     redirect_to :root
   end
 
   private
   def prototype_params
-    params.require(:prototype).permit(:copy, :concept, :image, :title)
+    params.require(:prototype).permit(:copy, :concept, :title, captured_images_attributes: [:img_url, :img_type]).merge({user_id: current_user.id})
   end
 end
