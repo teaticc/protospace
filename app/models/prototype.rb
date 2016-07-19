31d4700e68f798abe20f5d20c28e87dd2c4e57cc
nori <nori@nori-MacBook-Pro.local>
2016-07-19 18:59:53 +0900

    Make validation for prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,4 +3,6 @@ class Prototype < ActiveRecord::Base
   has_many :captured_images
   accepts_nested_attributes_for :captured_images
   validates :title, presence: true
+  validates :copy, presence: true
+  validates :concept, presence: true
 end
