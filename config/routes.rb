688d45d9710213bafa3e233682edb37d1e153d48
nori <nori@nori-MacBook-Pro.local>
2016-07-21 12:37:57 +0900

    Edit prototype index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resource :prototypes, only: [:index, :show, :new, :create]
+  resource :prototypes, only: [:index, :new, :create]
+  resources :prototypes, only: [:show]
   resources :users, only: [:edit, :show, :index, :update]
 end
