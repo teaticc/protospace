9121833f54bf7341f7fff904ba81ac83b460465c
yutaka karube <yutaka karube>
2016-07-21 20:56:19 +0900

    edit resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resource :prototypes, only: [:index, :new, :create]
-  resources :prototypes, only: [:show]
+  resources :prototypes, only: [:show,:index, :new, :create]
   resources :users, only: [:edit, :show, :index, :update]
 end
