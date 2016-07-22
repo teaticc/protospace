7284dd042d764cdc51c2fecbedd14c1014b72feb
nori <nori@nori-MacBook-Pro.local>
2016-07-22 15:02:20 +0900

    get image by method

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,4 +3,13 @@ class Prototype < ActiveRecord::Base
   has_many :captured_images, dependent: :destroy
   accepts_nested_attributes_for :captured_images
   validates :title, :copy, :concept , presence: true
+
+  def img_get(img_type)
+    if img_type == "main"
+      captured_images.main[0]
+    elsif img_type == "sub"
+      captured_images.sub
+    end
+  end
+
 end
