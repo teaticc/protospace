ac448f4eab6b7d0ab626f9839c3d734cf4644b52
nori <nori@nori-MacBook-Pro.local>
2016-07-25 21:08:05 +0900

    Good with ajax

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,14 +1,12 @@
 class GoodsController < ApplicationController
   def create
-    @good = Good.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @goods = Good.where(prototype_id: params[:prototype_id])
-    redirect_to root_path
+    Good.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @prototype = Prototype.find(params[:prototype_id])
   end
 
   def destroy
     good = Good.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     good.destroy
-    @goods = Good.where(prototype_id: params[:prototype_id])
-    redirect_to root_path
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
