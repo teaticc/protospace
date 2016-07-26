d9f794b3997be8a5d424f342f4e573461da2f37d
yutaka karube <yutaka karube>
2016-07-26 20:02:40 +0900

    make set_prototype

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,12 +1,17 @@
 class GoodsController < ApplicationController
+  before_action :set_prototype, only: :create
+
   def create
-    @prototype = Prototype.find(params[:prototype_id])
     @prototype.goods.create(user_id: current_user.id)
   end
 
   def destroy
     good = Good.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     good.destroy
+    set_prototype
+  end
+
+  def set_prototype
     @prototype = Prototype.find(params[:prototype_id])
   end
 end
