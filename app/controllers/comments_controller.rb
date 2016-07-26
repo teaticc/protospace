3ffe35076edc5b044ab3bfad899a22635f6a12cf
yutaka karube <yutaka karube>
2016-07-26 17:42:01 +0900

    comments function controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    Comment.create(comment_params)
     @prototype = Prototype.find(params[:prototype_id])
+    redirect_to root_path
   end
 
-  def destroy
-    Comment = Comment.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
-    comment.destroy
-    @prototype = Prototype.find(params[:prototype_id])
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge({user_id: current_user.id, prototype_id: params[:prototype_id]})
   end
 end
