ab4812d9389db16230476504454cc2cfda411cfb
nori <nori@nori-MacBook-Pro.local>
2016-07-28 16:30:16 +0900

    Use association to destroy

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,14 +1,15 @@
 class GoodsController < ApplicationController
   include Common
-  before_action :set_prototype, only: :create
+  before_action :set_prototype, only: [:create, :destroy]
 
   def create
     @prototype.goods.create(user_id: current_user.id)
   end
 
   def destroy
-    good = Good.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
-    good.destroy
+    # good = Good.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
+    # good.destroy
+    @prototype.goods.find_by(user_id: current_user.id).destroy
     set_prototype
   end
 end
