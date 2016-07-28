1474d41805518d11e1547b7c8d80de94ab632141
nori <nori@nori-MacBook-Pro.local>
2016-07-28 13:42:24 +0900

    Remove {} form .merge

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:comment).merge({user_id: current_user.id})
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id)
   end
 end
