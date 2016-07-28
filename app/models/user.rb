ecb822ce0032614f11f35d27134b5bf6faea8890
nori <nori@nori-MacBook-Pro.local>
2016-07-28 14:47:32 +0900

    Rewrite easily to read

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :prototypes
-  validates :name, presence: true
   has_many :goods
   has_many :comments
+  validates :name, presence: true
 end
