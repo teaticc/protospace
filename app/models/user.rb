f360b5f47efb44e30883326f1bbb685eb768d71d
nori <nori@nori-MacBook-Pro.local>
2016-07-29 12:51:52 +0900

    Add dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :prototypes
-  has_many :goods
-  has_many :comments
+  has_many :goods, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :name, presence: true
 end
