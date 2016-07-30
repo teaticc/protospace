663bdc06723ed1109165dc8a08d3bd0aeafc930b
nori <nori@nori-MacBook-Pro.local>
2016-07-30 15:17:57 +0900

    Add "js-" to class for javascript

--- app/assets/javascripts/add_fit_img.js
+++ app/assets/javascripts/add_fit_img.js
@@ -1,5 +1,5 @@
 $(document).on("turbolinks:load",function(){
-  $("img.add_fit_img").each(function(){
+  $("img.js-add_fit_img").each(function(){
     if (($(this).attr("src")) && ($(this).parent(/\.height.00/))){
       $(this).addClass("fit_img");
       $(this).parent(/\.height.00/).removeClass(function(index, className){
