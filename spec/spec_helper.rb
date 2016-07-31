bd54537f547c1657cc25dfd059100f50e6061bcf
nori <nori@nori-MacBook-Pro.local>
2016-07-31 20:38:08 +0900

    Preparation for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,10 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  # # コントローラーのテストで使う予定
+  # config.include Devise::TestHelpers, :type => :controller
+  # config.extend ControllerMacros, :type => :controller
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
