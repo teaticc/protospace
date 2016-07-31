7759afd24475c6ca16ec47cf80eefc442423cfac
nori <nori@nori-MacBook-Pro.local>
2016-07-31 20:39:09 +0900

    Add validation for captured_image

--- app/models/captured_image.rb
+++ app/models/captured_image.rb
@@ -2,4 +2,5 @@ class CapturedImage < ActiveRecord::Base
   mount_uploader :img_url, CapturedImageUploader
   enum img_type: { main: 0, sub: 1 }
   belongs_to :prototype
+  validates :img_url, presence: true
 end
