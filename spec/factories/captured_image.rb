aec0ada564a6bbaf9a66fb4ffe420eca2c949359
nori <nori@nori-MacBook-Pro.local>
2016-08-01 15:53:51 +0900

    Test with not image file

--- spec/factories/captured_image.rb
+++ spec/factories/captured_image.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :captured_image, class: CapturedImage do
-    img_type 0
+    img_type "main"
     img_url { File.open(File.join(Rails.root, 'spec/images/sample.png')) }
+
+    trait :with_txt_file do
+      after(:build) do |captured_image|
+        captured_image.img_url = File.open(File.join(Rails.root, 'spec/images/bad.txt'))
+      end
+    end
+
   end
 end
--- spec/models/captured_image_spec.rb
+++ spec/models/captured_image_spec.rb
@@ -4,16 +4,24 @@ describe CapturedImage do
 
   describe "validations" do
 
-    describe "with valid attributes" do
+    context "with valid attributes" do
       it "has a valid factory" do
         captured_image = build :captured_image
         expect(captured_image).to be_valid
       end
+    end
 
+    context "with invalid attributes" do
       it "is missing img_url" do
         captured_image = build :captured_image, img_url: nil
         captured_image.valid?
         expect(captured_image.errors[:img_url]).to include("can't be blank")
+      end
+
+      it "is invalid file" do
+        captured_image = build :captured_image, :with_txt_file
+        captured_image.valid?
+        expect(captured_image.errors[:img_url]).to include("You are not allowed to upload \"txt\" files, allowed types: jpg, jpeg, gif, png")
       end
     end
 
