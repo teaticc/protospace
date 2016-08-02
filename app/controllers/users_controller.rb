5c20338545d88b05d79ead80dd5a3ceb2d7b9e21
nori <nori@nori-MacBook-Pro.local>
2016-08-02 20:15:20 +0900

    Add control syntax for failure of update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,11 @@ class UsersController < ApplicationController
 
   def update
     # @user.update(user_params) なぜか機能しない
-     @user.update_attributes(name: user_params[:name], email: user_params[:email], member:user_params[:member], profile: user_params[:profile], avatar: user_params[:avatar], works: user_params[:works])
-    redirect_to root_path, notice: "successfully updated!"
+    if @user.update_attributes(name: user_params[:name], email: user_params[:email], member:user_params[:member], profile: user_params[:profile], avatar: user_params[:avatar], works: user_params[:works])
+      redirect_to root_path, notice: "successfully updated!"
+    else
+      render :edit
+    end
   end
 
   private
@@ -24,6 +27,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,:member, :profile, :avatar, :works)
+    params.require(:user).permit(:name, :email, :password, :member, :profile, :avatar, :works)
   end
+
 end
