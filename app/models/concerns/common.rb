edc4e6373db5b6fb4b75d2eebeb0d0eb56dc721b
nori <nori@nori-MacBook-Pro.local>
2016-08-02 16:13:27 +0900

    Add before action for not to sign in user

--- app/models/concerns/common.rb
+++ app/models/concerns/common.rb
@@ -4,4 +4,10 @@ module Common
   def set_prototype
     @prototype = Prototype.find(params[:prototype_id])
   end
+
+  def return_sign_out_user
+    unless user_signed_in?
+      redirect_to root_path, notice: "please sign in"
+    end
+  end
 end
