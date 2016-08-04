e611663ba81168984ca60a08186f9e59a94d06ae
nori <nori@nori-MacBook-Pro.local>
2016-08-04 13:35:54 +0900

    Test for comments controller (NOT finished)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
   include Common
   before_action :set_prototype, only: :create
+  before_action :return_sign_out_user, only: :create
+
   def create
     @prototype.comments.create(comment_params)
-    # N+1問題発生中（includesしても無理）
+    @comments = Comment.where(prototype_id: params[:prototype_id]).includes(:user)
   end
 
   private
