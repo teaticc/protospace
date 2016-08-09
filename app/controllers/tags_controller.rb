8c17ff62682032f91f7277e99f5a30f7cd8b1278
nori <nori@nori-MacBook-Pro.local>
2016-08-09 15:40:12 +0900

    Solve N + 1 problem

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,6 @@ class TagsController < ApplicationController
 
   def show
     @tag =  ActsAsTaggableOn::Tag.find(params[:id])
-    @prototypes = Prototype.tagged_with(@tag.name)
+    @prototypes = Prototype.tagged_with(@tag.name).includes(:user, :tags)
   end
 end
