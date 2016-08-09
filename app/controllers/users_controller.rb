047a1e5679453f49bd4261b46201ccbca9d44111
nori <nori@nori-MacBook-Pro.local>
2016-08-09 13:23:37 +0900

    To sign in again when user edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
+      bypass_sign_in @user
       redirect_to root_path, notice: "successfully updated!"
     else
       render :edit
